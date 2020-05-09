9f50f4200d15241131b0cbccb44287dd5f338760
FOXPP2047 <qlxhdl55@gmail.com>
2020-05-08 21:11:03 -0700

    Complete Blog Challenge with node express lodash body-parser

--- 016/ejs-challenge/app.js
+++ 016/ejs-challenge/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,13 +13,15 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+const posts = [];
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
-
 app.get("/", function(req, res) {
-  res.render("home", {homeText: homeStartingContent});
+  res.render("home", { homeText: homeStartingContent,
+                       posts: posts
+  });
 });
 
 app.get("/about", function(req, res) {
@@ -34,15 +37,27 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-  let postObj = {
+  const postObj = {
     title: req.body.postTitle,
     body: req.body.postBody
   };
+  posts.push(postObj);
 
+  res.redirect("/");
 });
 
+app.get("/posts/:title", function(req, res) {
+  const reqTitle = _.lowerCase(req.params.title);
+  posts.forEach(function(e) {
+    const storedTitle = _.lowerCase(e.title);
 
-
+    if(reqTitle === storedTitle) {
+      res.render("post", { title: e.title,
+                            body: e.body
+                          });
+    }
+  });
+});
 
 
 
